7fc00e7bf39f393727412195c98bf5c8a3b9838b
tatsushitoji <28854043+tatsushitoji@users.noreply.github.com>
2019-02-21 10:39:50 +0900

    chore(devDeps): introduce eslint

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  env: {
+    browser: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb',
+    'plugin:import-order/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: '.'
+  },
+  plugins: [
+    '@typescript-eslint',
+    'import-order',
+    'react-hooks',
+    'prettier',
+  ],
+  settings: {
+    'import/resolver': {
+      'node': {
+        'extensions': [
+          '.js', '.jsx', '.ts', '.tsx',
+        ],
+      },
+    }
+  },
+  rules: {
+    'import/no-extraneous-dependencies': [
+      'error',
+      { devDependencies: ['**/*.spec.tsx'] },
+    ],
+    'import/prefer-default-export': false,
+    'no-return-assign': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+    'react/prop-types': false,
+    'react-hooks/rules-of-hooks': 'error',
+  },
+};
