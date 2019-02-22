63b3552ba93396a7300feba5cd0dd3da49ee7090
tatsushitoji <28854043+tatsushitoji@users.noreply.github.com>
2019-02-22 11:13:33 +0900

    test: add moduleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,9 @@ module.exports = {
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^lodash-es$': 'lodash',
+  },
   // "snapshotSerializers": ["enzyme-to-json/serializer"],
   // setupTestFrameworkScriptFile: '<rootDir>setupTests.ts',
 };
