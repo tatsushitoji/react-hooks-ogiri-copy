04e8cfca2e9f67419543fea3e54a85b5723af078
tatsushitoji <28854043+tatsushitoji@users.noreply.github.com>
2019-02-22 16:02:23 +0900

    test: corresponding to rippleButton implementation

--- apps/ripple-button/App.spec.tsx
+++ apps/ripple-button/App.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render, cleanup } from 'react-testing-library';
+import { render, getByTestId, cleanup } from 'react-testing-library';
 import App from './App';
 
 afterEach(cleanup);
@@ -7,7 +7,11 @@ afterEach(cleanup);
 describe('ripple-button/App', () => {
   test('should render', () => {
     const { container } = render(<App />);
-
-    expect(container.textContent).toEqual('rippleButton');
+    const button1 = getByTestId(container, 'button1');
+    const button2 = getByTestId(container, 'button2');
+    const button3 = getByTestId(container, 'button3');
+    expect(button1.textContent).toBe('DOWNLOAD');
+    expect(button2.textContent).toBe('UPLOAD');
+    expect(button3.textContent).toBe('DOWNLOAD');
   });
 });
